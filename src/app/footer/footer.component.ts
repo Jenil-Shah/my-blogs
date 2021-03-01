d994e5e3eb0a361ca6bbba348766ca692875132c
Jenil-Shah <jenilshah757@gmail.com>
2021-02-28 22:58:14 -0500

    certain link changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import blogData from '../blogData.json';
+import {BlogPost} from '../BlogPost';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  posts: Array<BlogPost> = blogData.slice(0,3);
 
   constructor() { }
 
